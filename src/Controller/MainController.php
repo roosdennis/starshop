85dd5b59315b97a5188ad9b7a30f2a9bf2e6c69d
Dennis Roos <dennisroos87@gmail.com>
2024-11-06 16:34:22 +0100

    stap 4

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,19 +10,14 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function homepage(): Response
+    public function homepage(StarshipRepository $starshipRepository): Response
     {
-        $starshipCount = 457;
+        $starships = $starshipRepository->findAll();
 
-        $myShip = [
-            'name' => 'Jedi Starfighter',
-            'class' => 'Garden',
-            'captain' => 'Obi-Wan Kenobi',
-            'status' => 'under construction',
-        ];
+        $myShip = $starships[array_rand($starships)];
 
         return $this->render('main/homepage.html.twig', [
-            'numberOfStarships' => $starshipCount,
+            'starShips' => $starships,
             'myShip' => $myShip,
         ]);
     }
